cccc0cde3299d3db31f9d63626f933acc3dec9a5
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-04-27 18:16:20 +0300

    feat: добавить возможность запустить приложение в App

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,6 +1,10 @@
 package hexlet.code;
 
+import hexlet.code.models.Url;
+import hexlet.code.models.query.QUrl;
 import io.javalin.Javalin;
+
+import java.util.List;
 
 public final class App {
 
@@ -8,7 +12,18 @@ public final class App {
         Javalin app = Javalin.create(config -> {
             config.enableDevLogging(); // enable extensive development logging for http and websocket
         });
-        app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.get("/", ctx -> {
+            Url url = new Url("hexlet.io");
+            url.save();
+            ctx.result("Hello World");
+        });
+
+        app.get("/kek", ctx -> {
+            List<Url> list = new QUrl().findList();
+            ctx.result(list.toString());
+        });
+
         return app;
     }
 
